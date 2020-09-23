81b2cedf23de0fe058a85d1b4b667b0176f993b6
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2020-09-23 20:11:43 +0800

    Remove extra social links

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,6 @@ $(function () {
     video();
     gallery();
     table();
-    social();
     mobileMenu();
 });
 
@@ -203,36 +202,6 @@ function table() {
                 });
         });
     }
-}
-
-function social() {
-    "use strict";
-    var data = {
-        facebook: { name: "Facebook", icon: "facebook" },
-        twitter: { name: "Twitter", icon: "twitter" },
-        instagram: { name: "Instagram", icon: "instagram" },
-        dribbble: { name: "Dribbble", icon: "dribbble" },
-        behance: { name: "Behance", icon: "behance" },
-        github: { name: "GitHub", icon: "github-circle" },
-        linkedin: { name: "LinkedIn", icon: "linkedin" },
-        vk: { name: "VK", icon: "vk" },
-        rss: { name: "RSS", icon: "rss" },
-    };
-    var links = themeOptions.social_links;
-    var output = "";
-
-    for (var key in links) {
-        if (links[key] != "") {
-            output +=
-                '<a class="footer-social-item" href="' +
-                links[key] +
-                '" target="_blank"><i class="icon icon-' +
-                data[key]["icon"] +
-                '"></i></a>';
-        }
-    }
-
-    $(".footer-social").html(output);
 }
 
 function mobileMenu() {
